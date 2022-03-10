62aa28cd5e77a7ad3169cf02dbb38d2d5710de74
hnunezz <henrique.nunes@atlasinovacoes.com.br>
2022-03-09 22:41:32 -0300

    Feat: login layout v1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessComponent } from './access/access.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AccessComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccessComponent } from './access/access.component';
+import { LoginComponent } from './access/login/login.component';
+import { RegisterComponent } from './access/register/register.component';
+import { BannerComponent } from './access/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent,
+    LoginComponent,
+    RegisterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
