2d2d4294543a262a5ecc37a5b7644633ade63bce
hnunezz <henrique.nunes@atlasinovacoes.com.br>
2022-03-15 20:42:06 -0300

    Feat: storing data in the database

--- src/app/Service/auth.service.ts
+++ src/app/Service/auth.service.ts
@@ -1,13 +1,22 @@
 import { User } from "../models/user.model";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
+
 export class AuthService{
   public RegisterUser(user: User): void{
 
       const auth = getAuth();
+      const DTB = getDatabase()
 
       createUserWithEmailAndPassword(auth, user.email, user.password)
         .then((answer: any) => {
-          console.log(answer)
+           set(ref(DTB, `users/${user.name}`), {
+             name: user.name,
+             email: user.email,
+             user: user.user
+           });
+
+
         })
         .catch((error: Error) => {
           console.log(error)
